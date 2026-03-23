3a9be713e957abc800f084f2e714047a2f3fe31c
Manoelle <manoelle.ancion@gmail.com>
2026-03-23 15:21:24 +0100

    Intégration du hero header et de la section intro

--- front-page.php
+++ front-page.php
@@ -2,8 +2,33 @@
 
 <section class="front-page-hero">
 	<div class="front-page-hero-overlay"> </div>
-	<div class="home-hero">
+	<div class="front-page-hero-content"> 
+		<p class="front-page-hero-subtitle">Webdesign & développement Wordpress</p>
+		<h1 class="front-page-hero-title"> Je conçois des sites web <br> avec soin, <br> <span class="front-page-hero-opacity"> pensés pour des projets <br> qui ont du sens. </span> </h1>
+		<div class="front-page-hero-bottom"> 
+			<div>	
+				<a class="front-page-hero-bottom-projet" href="#projet">Voir mes projets</a>
+				<a class="front-page-hero-bottom-cta glass-button" href=#>Me contacter</a>
+			</div>
+			<p class="front-page-hero-bottom-author"> Manoëlle Ancion <br> <span class="front-page-hero-opacity"> Studio Endevenir </span></p>
+		</div>
 	</div>
 </section>
-
+<section class="front-page-intro">
+	<div class="front-page-intro-content">
+		<h2 class="front-page-intro-subtitle"> Ma philosophie </h2>
+		<p>“Studio Endevenir, ce sont des sites web conçus avec <span class="front-page-intro-accent">intention</span>, pensés pour refléter <span class="front-page-intro-accent">ton identité</span> et valoriser ce que tu fais.</p>
+		<p>Un espace digital créé <span class="front-page-intro-accent">avec soin</span>, pour mettre en lumière <span class="front-page-intro-accent">ton univers</span> et donner <span class="front-page-intro-accent">du sens</span> à ta présence en ligne.” </p>
+	</div>
+	<p class="front-page-intro-signature"> Manoëlle </p>
+</section>
+<section>
+	<h2> Projets</h2>
+</section>
+<section>
+	<h2> À propos</h2>
+</section>
+<section>
+	<h2> Contact</h2>
+</section>
 <?php get_footer(); ?>
